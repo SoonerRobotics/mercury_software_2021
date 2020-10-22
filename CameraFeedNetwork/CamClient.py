6aaf1dbdbce4b030ae23cb14245bb471e9b9f661
Anthony Nguyen <anthonynguyenlaas@gmail.com>
2020-10-22 14:37:24 -0500

    Updated some files

--- CameraFeedNetwork/CamClient.py
+++ CameraFeedNetwork/CamClient.py
@@ -1,25 +1,30 @@
+###
+#  Deprecated.
+###
+
 import cv2, socket, pickle, struct
 import numpy as np
 
+def main():
+    videoFeed = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-videoFeed = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(("127.0.0.1", 25565))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(("127.0.0.1", 25565))
+        while True:
+            _, frame = videoFeed.read()
+            frame = cv2.resize(frame, (640, 480))
 
-    while True:
-        _, frame = videoFeed.read()
+            # encoding numpy array from "frame"
+            data = pickle.dumps(frame)
 
-        # encoding numpy array from "frame"
-        data = pickle.dumps(frame)
+            dataSize = len(data)
 
-        dataSize = len(data)
+            # encodes the size of the data
+            encodedSize = struct.pack("Q", dataSize)
 
-        # encodes the size of the data
-        encodedSize = struct.pack("Q", dataSize)
+            # sends size of data and data itself over the socket
+            s.sendall(encodedSize + data)
 
-        # sends size of data and data itself over the socket
-        s.sendall(encodedSize + data)
-
-
-#https://stackoverflow.com/questions/30988033/sending-live-video-frame-over-network-in-python-opencv
+if __name__ == "__main__":
+    main()
--- CameraFeedNetwork/CamServer.py
+++ CameraFeedNetwork/CamServer.py
@@ -1,51 +1,67 @@
+###
+#  Deprecated.
+###
+
 import cv2, socket, pickle, struct
+import numpy as np
+import time
+
+from CameraFeedNetwork import VideoFeedGUI
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        dataBytes = b''
+
+        # used for finding size from encoded data
+        loadSize = struct.calcsize("Q")
+
+        # normal socket stuff
+        s.bind(("0.0.0.0", 25565))
+        s.listen()
+
+        # data stuff over socket
+        connection, address = s.accept()
+
+        print("socket ready")
+
+        with connection:
+            print("client connected. ip is: ", address)
+
+            while True:
+
+                # scraping the data size from encoded socket connection
+                while (len(dataBytes) < loadSize):
+                    dataBytes += connection.recv(4096)
+
+                # decoding the data about data size
+                dataSize = struct.unpack("Q", dataBytes[:loadSize])[0]
+
+                # disregard dataSize, now ready for encoded data
+                dataBytes = dataBytes[loadSize:]
+
+                # keeps on receiving data until it hits the "pre-written" data size
+                while (len(dataBytes) < dataSize):
+                    dataBytes += connection.recv(4096)
+
+                # separating encoded data itself from any extra stuff sent over
+                frameData = dataBytes[:dataSize]
+                dataBytes = dataBytes[dataSize:]
+
+                # extracting frame from compression/encoding
+                frame = pickle.loads(frameData)
+
+                cv2.imwrite("frame.jpg", frame)
+                time.sleep(0.01)
+
+                #return frame
+
+                #cv2.imshow("videoFeed", frame)
+                #cv2.waitKey(1)
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    dataBytes = b''
 
-    # used for finding size from encoded data
-    loadSize = struct.calcsize("Q")
-
-    # normal socket stuff
-    s.bind(("0.0.0.0", 25565))
-    s.listen()
-
-    # data stuff over socket
-    connection, address = s.accept()
-
-    print("socket ready")
-
-    with connection:
-        print("client connected. ip is: ", address)
-
-        while True:
-
-            # scraping the data size from encoded socket connection
-            while (len(dataBytes) < loadSize):
-                dataBytes += connection.recv(4096)
-
-            # decoding the data about data size
-            dataSize = struct.unpack("Q", dataBytes[:loadSize])[0]
-
-            # disregard dataSize, now ready for encoded data
-            dataBytes = dataBytes[loadSize:]
-
-            # keeps on receiving data until it hits the "pre-written" data size
-            while (len(dataBytes) < dataSize):
-                dataBytes += connection.recv(4096)
-
-            # separating encoded data itself from any extra stuff sent over
-            frameData = dataBytes[:dataSize]
-            dataBytes = dataBytes[dataSize:]
-
-            # extracting frame from compression/encoding
-            frame = pickle.loads(frameData)
-
-            #height, width, _ = frame.shape
-            #cv2.putText(frame, "yeet", (.25 * height, .25 * width), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 3)
-
-            cv2.imshow("videoFeed", frame)
-            cv2.waitKey(1)
-
-    #return frame
+if __name__ == "__main__":
+      main()
+#frameA = socketServer()
+#print(frameA)
+#VideoFeedGUI.GUI(frame)
