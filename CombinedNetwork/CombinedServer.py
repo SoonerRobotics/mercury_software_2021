4d2b15c9c10bc2ba62d43b82c9e404a01e6ad067
Anthony Nguyen <44672084+anthony-nguyen-04@users.noreply.github.com>
2021-04-20 18:15:08 -0500

    Create CombinedServer.py

---
+++ CombinedNetwork/CombinedServer.py
@@ -0,0 +1,48 @@
+###
+# "Robot" sending data to computer "Server"
+###
+
+import cv2, socket, pickle, struct
+from multiprocessing import Process
+import numpy as np
+
+
+def main():
+    # Webcam on
+    videoFeed = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # setting up the socket
+        s.bind(("0.0.0.0", 25565))
+        s.listen()
+
+        connection, address = s.accept()
+
+        while True:
+            # keyData = connection.recv(1024)
+            # print("data: ", (str(keyData))[1:])
+
+            # camera feed
+            _, frame = videoFeed.read()
+            frame = cv2.resize(frame, (640, 480))
+
+            # encoding/compressing numpy array from "frame"
+            data = pickle.dumps(frame)
+
+            dataSize = len(data)
+
+            # encodes the size of the data
+            encodedSize = struct.pack("Q", dataSize)
+
+            # sends size of data and data itself over the socket
+            connection.sendall(encodedSize + data)
+
+            # receiving controller input
+            dataBytes = connection.recv(4096)
+
+            dict = pickle.loads(dataBytes)
+            print(dict)
+
+
+if __name__ == "__main__":
+    main()
